9691b188c61f15c65c77e6e11481461483488df9
Martin Hofhanzl <hofhanzlmartin@gmail.com>
2017-04-29 21:03:08 +0100

    Concatenated JavaScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+	concat = require('gulp-concat');
+
+var jsSources = ['components/scripts/script.js'];
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js/'))
+});
