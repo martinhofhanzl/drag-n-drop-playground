9ff8a793e1992e32640e31c6bd2094750c9f8199
Martin Hofhanzl <hofhanzlmartin@gmail.com>
2017-04-30 09:14:45 +0100

    Added sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp')
 	concat = require('gulp-concat')
-	browserify = require('gulp-browserify');
+	browserify = require('gulp-browserify')
+	sass = require('gulp-sass')
 
 var jsSources = [
 	'components/scripts/modernizr-custom.js',
 	'components/scripts/script.js'];
+
+gulp.task('sass', function() {
+	gulp.src('components/sass/*.scss')
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+		.pipe(gulp.dest('builds/development/css/'))
+});
 
 gulp.task('js', function() {
 	gulp.src(jsSources)
